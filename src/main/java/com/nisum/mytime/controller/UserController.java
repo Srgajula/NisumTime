423a96095fbe079e7691e042529002b1703e9d19
Achuth Goud <adomakonda@nisum.com>
2017-12-11 11:33:26 +0530

    MT-006 [Achuth] : Fetching data to controller from Sso Login

--- src/main/java/com/nisum/mytime/controller/UserController.java
+++ src/main/java/com/nisum/mytime/controller/UserController.java
@@ -19,25 +19,31 @@ import com.nisum.mytime.service.UserService;
 @RequestMapping("/user")
 public class UserController {
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@RequestMapping(value = "employee/{emailId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<EmployeeRoles>> getEmployeeRole(@PathVariable("emailId") String emailId) throws MyTimeException {
-		List<EmployeeRoles> employeesRoles = userService.getEmployeesRole();
-		return new ResponseEntity<>(employeesRoles, HttpStatus.OK);
-	}
-	
-	@RequestMapping(value = "/employeesDataSave", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Boolean> employeesDataSave() throws MyTimeException {
-		Boolean result = userService.fetchEmployeesData();
-		return new ResponseEntity<>(result, HttpStatus.OK);
-	}
+    @RequestMapping(value = "employee/{emailId}", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<EmployeeRoles>> getEmployeeRole(
+            @PathVariable("emailId") String emailId) throws MyTimeException {
+        List<EmployeeRoles> employeesRoles = userService.getEmployeesRole();
+        return new ResponseEntity<>(employeesRoles, HttpStatus.OK);
+    }
 
-	@RequestMapping(value = "/assigingEmployeeRole", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<String> assigingEmployeeRole(EmployeeRoles employeeRoles) throws MyTimeException {
-		userService.assigingEmployeeRole(employeeRoles);
-		return new ResponseEntity<>("", HttpStatus.OK);
-	}
+    @RequestMapping(value = "/employeesDataSave", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Boolean> employeesDataSave() throws MyTimeException {
+        Boolean result = userService.fetchEmployeesData();
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/assigingEmployeeRole",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> assigingEmployeeRole(
+            EmployeeRoles employeeRoles) throws MyTimeException {
+        userService.assigingEmployeeRole(employeeRoles);
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
 
 }
--- src/main/webapp/controllers/LoginController.js
+++ src/main/webapp/controllers/LoginController.js
@@ -1,4 +1,4 @@
-myApp.controller("loginController",function($scope, myFactory, $compile){
+myApp.controller("loginController",function($scope,$http, myFactory, $compile){
 	var menuItems = myFactory.getMenuItems();
 	$scope.authenticate = function(role){
 		
@@ -23,6 +23,10 @@ myApp.controller("loginController",function($scope, myFactory, $compile){
 		myFactory.setMenuItems(menuItems);
 		myFactory.setTemplateUrl("templates/employee.html");
 		
+		$scope.goToEmploy = function(){
+			$location.path("templates/employee.html");
+		};
+		
 		//Redirecting to home page after logging in successfully
 		var element = document.getElementById('home');
 		var path = "'templates/home.html'";
@@ -30,6 +34,19 @@ myApp.controller("loginController",function($scope, myFactory, $compile){
 		var newTemplate = angular.element(element);
 		$('#home').html(newTemplate);
 		$compile($('#home'))($scope)
-	}
+	};
+	$scope.onSignIn = function(googleUser){
+		 var profile = googleUser.getBasicProfile();
+	      console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+	      console.log('Full Name: ' + profile.getName());
+	      console.log('Given Name: ' + profile.getGivenName());
+	      console.log('Family Name: ' + profile.getFamilyName());
+	      console.log("Image URL: " + profile.getImageUrl());
+	      console.log("Email: " + profile.getEmail());
+
+	      // The ID token you need to pass to your backend:
+	      var id_token = googleUser.getAuthResponse().id_token;
+	      console.log("ID Token: " + id_token);
+	};
 	
 });
