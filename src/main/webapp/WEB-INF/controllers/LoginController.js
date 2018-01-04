077dfd65b977a823bf3d070f4a03990a36e127de
Rajeshekar <Rsayannagari-nisum-com>
2018-01-04 11:24:04 +0530

    Added profile changes

--- src/main/webapp/WEB-INF/controllers/LoginController.js
+++ src/main/webapp/WEB-INF/controllers/LoginController.js
@@ -228,25 +228,17 @@ myApp.controller("loginController",function($scope, myFactory, $compile, $window
 			menuItems.push({"menu" : "Attendance Report","icon" : "fa fa-bar-chart fa-2x","path" : "templates/attendanceReport.html"});
 			menuItems.push({"menu" : "Shift Details","icon" : "fa fa-superpowers fa-2x","path" : "templates/shiftdetails.html"});
 			menuItems.push({"menu" : "ReSync Data","icon" : "fa fa-recycle fa-2x","path" : "templates/resyncData.html"});
-<<<<<<< HEAD
-		    menuItems.push({"menu" : "My Profile","icon" : "fa fa-user-circle-o fa-2x","path" : "templates/profile.html"});
-			
-=======
->>>>>>> branch 'master' of https://github.com/srgajula-nisum-com/NisumTime
+            menuItems.push({"menu" : "My Profile","icon" : "fa fa-address-card-o fa-2x","path" : "templates/profile.html"});
 		}else if(role == "Manager"){
 			menuItems.push({"menu" : "My Details","icon" : "fa fa-indent fa-2x","path" : "templates/employee.html"});
 			menuItems.push({"menu" : "Reportee Details","icon" : "fa fa-users fa-2x","path" : "templates/reportees.html"});
 			menuItems.push({"menu" : "Manage Team","icon" : "fa fa-sitemap fa-2x","path" : "templates/projectDetails.html"});
-<<<<<<< HEAD
 			menuItems.push({"menu" : "View Projects","icon" : "fa fa-compass fa-2x","path" : "templates/viewProjects.html"});
-			menuItems.push({"menu" : "My Profile","icon" : "fa fa-user-circle-o fa-2x","path" : "templates/profile.html"});
-=======
-			menuItems.push({"menu" : "View Projects","icon" : "fa fa-tasks fa-2x","path" : "templates/viewProjects.html"});
->>>>>>> branch 'master' of https://github.com/srgajula-nisum-com/NisumTime
+			menuItems.push({"menu" : "My Profile","icon" : "fa fa-address-card-o fa-2x","path" : "templates/profile.html"});
 		}else if(role == "Employee"){
 			menuItems.push({"menu" : "My Details","icon" : "fa fa-indent fa-2x","path" : "templates/employee.html"});
 			menuItems.push({"menu" : "My Team","icon" : "fa fa-futbol-o fa-2x","path" : "templates/myTeam.html"});
-			menuItems.push({"menu" : "My Profile","icon" : "fa fa-user-circle-o fa-2x","path" : "templates/profile.html"});
+			menuItems.push({"menu" : "My Profile","icon" : "fa fa-address-card-o fa-2x","path" : "templates/profile.html"});
 		}else if(role == "HR Manager" || role == "Director"){
 			menuItems.push({"menu" : "My Details","icon" : "fa fa-indent fa-2x","path" : "templates/employee.html"});
 			menuItems.push({"menu" : "Employee Details","icon" : "fa fa-users fa-2x","path" : "templates/employees.html"});
@@ -256,12 +248,12 @@ myApp.controller("loginController",function($scope, myFactory, $compile, $window
 			menuItems.push({"menu" : "Manage Projects","icon" : "fa fa-tasks fa-2x","path" : "templates/projects.html"});
 			menuItems.push({"menu" : "Attendance Report","icon" : "fa fa-bar-chart fa-2x","path" : "templates/attendanceReport.html"});
 			menuItems.push({"menu" : "Shift Details","icon" : "fa fa-superpowers fa-2x","path" : "templates/shiftdetails.html"});
-			menuItems.push({"menu" : "My Profile","icon" : "fa fa-user-circle-o fa-2x","path" : "templates/profile.html"});
+			menuItems.push({"menu" : "My Profile","icon" : "fa fa-address-card-o fa-2x","path" : "templates/profile.html"});
 		}else if(role == "Lead"){
 			menuItems.push({"menu" : "My Details","icon" : "fa fa-indent fa-2x","path" : "templates/employee.html"});
 			menuItems.push({"menu" : "Reportee Details","icon" : "fa fa-users fa-2x","path" : "templates/reportees.html"});
 			menuItems.push({"menu" : "Manage Team","icon" : "fa fa-sitemap fa-2x","path" : "templates/projectDetails.html"});
-			menuItems.push({"menu" : "My Profile","icon" : "fa fa-user-circle-o fa-2x","path" : "templates/profile.html"});
+			menuItems.push({"menu" : "My Profile","icon" : "fa fa-address-card-o fa-2x","path" : "templates/profile.html"});
 		}
 		
 		myFactory.setMenuItems(menuItems);
